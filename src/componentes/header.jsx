d8283dd83c98e1e90f36b545e2f0c0ea6bc8f50e
Idania de Jesus Guerrero Fonseca <iguerrerof@outlook.com>
2022-10-10 11:35:32 -0500

    adding meetingList componente

---
+++ src/componentes/header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <h1>
+            Estoy en el header, es mi primer componente 
+        </h1>
+    )
+}
+
+export default Header(); 
---
+++ src/componentes/listadoPaciente.jsx
@@ -0,0 +1,10 @@
+function listadoPaciente(){
+    return (
+        <div class="col-md-6">
+            <h2 id="administra" class="mb-4">Administra tus Citas</h2>
+            <ul id="citas" class="list-group lista-citas"></ul>
+        </div>
+    )
+}
+
+export default listadoPaciente(); 
